23f8a6b6acc91c45a0447c297b540c66c847c222
natalieflcn <natalie.dflcn@gmail.com>
2025-02-14 11:33:04 -0500

    Completed Has Loop Leetcode Exercise

--- script.js
+++ script.js
@@ -142,16 +142,183 @@ class LinkedList {
 
     return slow;
   }
+
+  //REMOVE DUPLICATES
+  //   Implement a member function called removeDuplicates() that removes all duplicate nodes from the linked list based on their values.
+
+  // Note: this linked list class does NOT have a tail which will make this method easier to implement.
+
+  // Output:
+
+  // The function should modify the linked list in-place, removing all nodes with duplicate values.
+
+  // Constraints:
+
+  // You are allowed to use the JavaScript Set data structure to keep track of unique node values.
+
+  // Example 1:
+
+  // Suppose you have a LinkedList object, list, with the following values:
+  // 1 -> 2 -> 3 -> 2 -> 1 -> 4
+
+  // After calling the removeDuplicates() function:
+
+  // list.removeDuplicates();
+  // The linked list should now have the following values: 1 -> 2 -> 3 -> 4
+
+  // Example 2:
+
+  // Now suppose you have a LinkedList object, list, with the following values:
+  // 3 -> 3 -> 3
+
+  // After calling the removeDuplicates() function:
+
+  // list.removeDuplicates();
+  // The linked list should now have the following value: 3
+
+  // Remember to update the length.
+
+  removeDuplicates() {
+    let fast = this.head;
+    let slow = this.head;
+    let mySet = new Set();
+
+    while (fast !== null) {
+      if (fast.next === null) {
+        if (mySet.has(fast.value)) {
+          slow.next = null;
+          this.length--;
+          break;
+        } else {
+          mySet.add(fast.value);
+          break;
+        }
+      }
+
+      if (mySet.has(fast.value)) {
+        //DUPLICATE
+        fast = fast.next;
+        slow.next = fast;
+        this.length--;
+      } else {
+        //UNIQUE VALUE
+        mySet.add(fast.value);
+        fast = fast.next;
+
+        if (!mySet.has(fast.value)) {
+          slow = slow.next;
+        }
+      }
+    }
+  }
+
+  //HAS LOOP
+  // Write a method called hasLoop that is part of the linked list class.
+
+  // The method should be able to detect if there is a cycle or loop present in the linked list.
+
+  // You are required to use Floyd's cycle-finding algorithm (also known as the "tortoise and the hare" algorithm) to detect the loop.
+
+  // This algorithm uses two pointers: a slow pointer and a fast pointer. The slow pointer moves one step at a time, while the fast pointer moves two steps at a time. If there is a loop in the linked list, the two pointers will eventually meet at some point. If there is no loop, the fast pointer will reach the end of the list.
+
+  // The method should follow these guidelines:
+
+  // Create two pointers, slow and fast, both initially pointing to the head of the linked list.
+
+  // Traverse the list with the slow pointer moving one step at a time, while the fast pointer moves two steps at a time.
+
+  // If there is a loop in the list, the fast pointer will eventually meet the slow pointer. If this occurs, the method should return true.
+
+  // If the fast pointer reaches the end of the list or encounters a null value, it means there is no loop in the list. In this case, the method should return false.
+
+  // Output:
+
+  // Return true if the linked list has a loop.
+
+  // Return false if the linked list does not have a loop.
+
+  // Constraints:
+
+  // You are not allowed to use any additional data structures (such as arrays) or modify the existing data structure.
+
+  // You can only traverse the linked list once.
+
+  // If your Linked List contains a loop, it indicates a flaw in its implementation. This situation can manifest in several ways:
+
+  hasLoop() {
+    if (!this.head) return false;
+
+    let slow = this.head;
+    let fast = this.head;
+
+    do {
+      slow = slow.next;
+
+      if (fast === null || fast.next === null) {
+        return false;
+      }
+
+      fast = fast.next.next;
+    } while (fast !== slow);
+
+    return true;
+  }
 }
 
-const myLinkedList = new LinkedList(1);
-myLinkedList.push(2);
+//TEST CASE 1
+let myLinkedList = new LinkedList(4);
+myLinkedList.push(11);
 myLinkedList.push(3);
-myLinkedList.push(4);
-myLinkedList.push(5);
-myLinkedList.printList();
-console.log(myLinkedList.findKthFromEnd(2));
+myLinkedList.push(23);
+myLinkedList.push(7);
 
-myLinkedList.push(6);
+console.log("Original list (without loop):");
 myLinkedList.printList();
-console.log(myLinkedList.findKthFromEnd(2));
+
+console.log(`\nHas loop? ${myLinkedList.hasLoop()}`);
+
+// Create a loop by linking tail to the second node
+myLinkedList.tail.next = myLinkedList.head.next;
+
+//console.log(`\nHas loop? ${myLinkedList.hasLoop()}`);
+
+//TEST 2
+let myLinkedList2 = new LinkedList(4);
+myLinkedList2.push(11);
+myLinkedList2.push(3);
+myLinkedList2.push(23);
+myLinkedList2.push(7);
+
+console.log("Original list (without loop):");
+myLinkedList2.printList();
+
+console.log(`\nHas loop? ${myLinkedList2.hasLoop()}`);
+
+// Create a loop by linking tail to the head
+myLinkedList2.tail.next = myLinkedList2.head;
+
+//console.log(`\nHas loop? ${myLinkedList2.hasLoop()}`);
+
+// //TEST 3
+// let myLinkedList3 = new LinkedList(4);
+// myLinkedList3.push(11);
+// myLinkedList3.push(3);
+// myLinkedList3.push(23);
+// myLinkedList3.push(7);
+
+// console.log("Original list (without loop):");
+// myLinkedList2.printList();
+
+// console.log(`\nHas loop? ${myLinkedList3.hasLoop()}`);
+
+// //Disconnecting the tail from the linked list
+// let pointer = myLinkedList3.head;
+
+// while (pointer !== null) {
+//   if (pointer.next === myLinkedList3.tail) {
+//     pointer.next = myLinkedList3.head;
+//   }
+//   pointer = pointer.next;
+// }
+
+// console.log(`\nHas loop? ${myLinkedList3.hasLoop()}`);
