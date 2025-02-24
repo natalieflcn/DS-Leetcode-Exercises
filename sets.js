ac02084e8ba941d8cc5cc4c127c378aad6100bd1
natalieflcn <natalie.dflcn@gmail.com>
2025-02-24 10:54:24 -0500

    Completed hasUniqueChars Leetcode Exercise

--- sets.js
+++ sets.js
@@ -1,23 +1,8 @@
 // 57) Set: Remove Duplicates
 // Your task is to write a function called removeDuplicates that takes a list of numbers or letters as input.
-
 // This list can have some items that appear more than once.
-
 // Your function should return a new list where each number or letter only appears once.
-
 // You can assume the list only contains numbers or letters.
-
-// Example
-// Input: [1, 2, 2, 3, 4, 4, 4]
-// Output: [1, 2, 3, 4]
-
-// Requirements
-
-// The function should take a list as an input.
-
-// The function should return a new list.
-
-// Each item in the new list should only appear once.
 
 function removeDuplicates(input) {
   const list = new Set();
@@ -30,3 +15,32 @@ function removeDuplicates(input) {
 }
 
 console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 4]));
+
+// 56) Set: Has Unique Chars ( ** Interview Question)
+// Your task is to write a function named hasUniqueChars that takes a string as input. Your function should check if all the characters in the string are unique or not. In other words, no character should appear more than once in the string.
+
+// Example
+// Input: "hello"
+// Output: false
+
+// In this example, the letter 'l' appears two times in the word "hello". So, the function should return false.
+
+function hasUniqueChars(string) {
+  const list = new Set();
+
+  for (let char of string) {
+    if (list.has(char)) {
+      return false;
+    }
+    list.add(char);
+  }
+
+  return true;
+}
+
+console.log(hasUniqueChars("world"));
+// Notes
+
+// The function should consider upper-case and lower-case letters as different. For example, 'A' and 'a' should be considered unique.
+
+// An empty string should return true as it doesn't have any repeated characters.
