1196ce684e65aa612c387ef4dc093ba82e1a9c9d
natalieflcn <natalie.dflcn@gmail.com>
2025-02-28 13:14:30 -0500

    Implemented Graph Data Structure

--- graphs.js
+++ graphs.js
@@ -11,4 +11,51 @@ class Graph {
 
     return false;
   }
+
+  addEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex1].push(vertex2);
+      this.adjacencyList[vertex2].push(vertex1);
+      return true;
+    }
+
+    return false;
+  }
+
+  removeEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+        (v) => v !== vertex2
+      );
+      this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+        (v) => v !== vertex1
+      );
+      return true;
+    }
+    return false;
+  }
+
+  removeVertex(vertex) {
+    if (!this.adjacencyList[vertex]) return undefined;
+
+    while (this.adjacencyList[vertex.length]) {
+      let temp = this.adjancencyList[vertex].pop();
+      this.removeEdge(vertex, temp);
+    }
+
+    delete this.adjacencyList[vertex];
+
+    return this;
+  }
 }
+
+const myGraph = new Graph();
+//Adding Vertices
+myGraph.addVertex("N");
+myGraph.addVertex("A");
+myGraph.addVertex("T");
+
+//Adding Edges
+myGraph.addEdge("N", "A");
+
+console.log(myGraph);
